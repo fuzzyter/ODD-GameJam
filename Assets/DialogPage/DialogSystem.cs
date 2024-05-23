6002ced4d46cf73c1fd5d32bdae3b7b869a6bbc8
fuzzyter <165622596+fuzzyter@users.noreply.github.com>
2024-05-24 05:59:20 +0900

    DialogSystem_중간저장
    스크립트 출력 확인

--- Assets/DialogPage/DialogSystem.cs
+++ Assets/DialogPage/DialogSystem.cs
@@ -20,7 +20,13 @@ public class DialogSystem : MonoBehaviour
     private int currentLineIndex = 0;
 
     void Start()
-    {
+    {/*
+        // 이전 진행 상황이 저장되어 있으면 불러오기
+        if (PlayerPrefs.HasKey("CurrentLineIndex"))
+        {
+            currentLineIndex = PlayerPrefs.GetInt("CurrentLineIndex");
+        }
+        */
         if (CSVRead.doubleChatList != null && CSVRead.doubleChatList.Length > 0)
         {
             DisplayLine();
@@ -38,17 +44,27 @@ public class DialogSystem : MonoBehaviour
             }
         }
     }
+    /*
+    void OnDisable()
+    {
+        // 진행 상황 저장
+        PlayerPrefs.SetInt("CurrentLineIndex", currentLineIndex);
+        PlayerPrefs.Save();
+    }*/
 
     void DisplayLine()
     {
         string speakerName = CSVRead.doubleChatList[currentLineIndex, 1];
         string illustrationName = CSVRead.doubleChatList[currentLineIndex, 2];
         string dialogueLine = CSVRead.doubleChatList[currentLineIndex, 3];
-        string isChoice = CSVRead.doubleChatList[currentLineIndex, 4];
+        string ChoiceT01 = CSVRead.doubleChatList[currentLineIndex, 4];
+        string ChoiceT02 = CSVRead.doubleChatList[currentLineIndex, 5];
         string backgroundName = CSVRead.doubleChatList[currentLineIndex, 6];
 
         nameText.text = speakerName;
         dialogueText.text = dialogueLine;
+        ChoiceText01.text = ChoiceT01;
+        ChoiceText02.text = ChoiceT02;
 
         Choice01.gameObject.SetActive(false);
         Choice02.gameObject.SetActive(false);
@@ -84,16 +100,18 @@ public class DialogSystem : MonoBehaviour
             BlackImage.gameObject.SetActive(true);
             backgroundImage.sprite = null;
         }
-        /*
+       
         //선택지가 존재한다면 선택창/검은배경 표시
-        if (!string.IsNullOrEmpty(isChoice))
+        if (!string.IsNullOrEmpty(ChoiceT01))
         {
-            Debug.Log(isChoice);
+            Debug.Log(ChoiceT01);
             Choice01.gameObject.SetActive(true);
             Choice02.gameObject.SetActive(true);
             ChoiceText01.gameObject.SetActive(true);
             ChoiceText02.gameObject.SetActive(true);
             BlackImage.gameObject.SetActive(true);
+
+
         }
 
         else //선택지가 공란이면 선택창X
@@ -103,6 +121,12 @@ public class DialogSystem : MonoBehaviour
             ChoiceText01.gameObject.SetActive(false);
             ChoiceText02.gameObject.SetActive(false);
         }
-        */
+
+        //선택지내용넣는코드ㅡㅡ
+        //선택지따라코드건너뛰기(인덱스번호 이용)(이 코드에 클릭이벤트 추가)
+
+        //변수따라 다른csv실행(이거는 dialogsystem, csvread수정) 
+        //선택지 고르면 호감도 리턴(이건근데 코드끝나고 해도 되려나?)
+     
     }
 }
